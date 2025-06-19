3c0b0a8678085d2e3dcfd30b94ad8dfb6bd9490a
AleContePhisics <contale30@gmail.com>
2025-06-19 11:14:13 +0200

    Add files via upload

---
+++ SOURCE/GenAlgorithm.cpp
@@ -0,0 +1,440 @@
+# include "GenAlgorithm.h"
+# include <iomanip>
+# include <unordered_set>
+# include <mpi.h>
+
+
+double Dist(double x1,double y1,double x2,double y2){
+    double L;
+    if(x1==x2 & y1==y2)L=0;
+    L=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+    return L;
+}
+
+void Chromosome :: SingleChromoShow(){
+    for(int i=0;i<n_genes;i++){
+        cout<<GetGene(i)<<endl;
+    }
+}
+
+unsigned int Chromosome :: GetGene(unsigned int i){
+    if(i<n_genes)return Chromo[i];
+    else if(i==n_genes) return Chromo[0];
+    else {cerr<<"Error gene out of bounds, gene ="<<i<<endl;
+        exit(1);
+    }
+}
+
+void Chromosome :: Check() {
+    if (Chromo[0]!=1) {
+        cerr<<"Error: Initial gene is not 1."<<endl;
+        exit(1);
+    }
+    unordered_set<unsigned int> seen;
+    for (unsigned int gene:Chromo) {
+        if (!seen.insert(gene).second) {
+            cerr<<"Error: Duplicate gene found"<<endl;
+            exit(1);
+        }
+    }
+}
+
+void Chromosome :: Permutation(Random& rnd){
+    unsigned int r1=rnd.RandInt(1,n_genes-1);
+    unsigned int r2=rnd.RandInt(1,n_genes-1);
+
+    if(r1==r2) return;
+    double x;
+    x=GetGene(r1);
+    SetGene(GetGene(r2),r1);
+    SetGene(x,r2);
+    Check();
+                                      
+}
+
+void Chromosome :: Shift(Random& rnd){
+    rotate(Chromo.begin()+1,Chromo.end()-1,Chromo.end());
+    Check();
+}
+
+Population::Population(unsigned int m,unsigned int n){     //set the dimension of chromo(path) and genes(cities)
+    Popul.resize(m);
+    n_chromo=m;
+    n_genes=n;
+    for(int i=0;i<n_chromo;i++){
+        Popul[i]=Chromosome(n);
+    }
+    M.set_size(n_genes,n_genes);
+};
+
+void Chromosome :: Contamination(unsigned int i,Random& rnd){
+    unsigned int r;
+    do{
+        r=rnd.RandInt(1+i,n_genes-i-1);
+    }while(r%2!=0);
+    //cout<<"r="<<r<<endl;
+    for(int j=0;j<i;j++){
+        swap(Chromo[r-(i-j)],Chromo[r+j+1]);
+    }
+    Check();
+}
+
+void Chromosome::Inversion(Random& rnd) {
+    unsigned int maxj=(n_genes-1)/2;
+    unsigned int j=rnd.RandInt(1,maxj);
+    unsigned int i=rnd.RandInt(j+1,n_genes-j);
+
+    for (unsigned int k=0;k<j;k++) {
+        swap(Chromo[i-j+k],Chromo[i+k]);
+    }
+    Check();
+}
+
+void Population::Initialization() {
+    int p1 = 0, p2 = 0;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // ottieni il rank
+
+    // Leggi la riga del file Primes corrispondente al rank
+    ifstream Primes("../INPUT/Primes");
+    if (!Primes.is_open()) {
+        cerr << "[Rank " << rank << "] Errore apertura file Primes!" << endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    for (int i = 0; i <= rank; ++i) {
+        if (!(Primes >> p1 >> p2)) {
+            cerr << "[Rank " << rank << "] Errore lettura riga " << rank << " da Primes!" << endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+    Primes.close();
+
+    // Lettura seme
+    int seed[4];
+    ifstream Seed("../INPUT/seed.in");
+    if (!Seed.is_open()) {
+        cerr << "[Rank " << rank << "] Errore apertura file seed.in!" << endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    Seed >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+    Seed.close();
+
+    rnd.SetRandom(seed, p1, p2);
+
+    // Inizializza popolazione
+    for (int i = 0; i < n_genes; i++) {
+        Popul[0].SetGene(i + 1, i);  // primo cromosoma ordinato
+    }
+
+    for (int i = 1; i < n_chromo; i++) {
+        Popul[i] = Popul[0];
+    }
+
+    for (int i = 1; i < n_chromo; i++) {
+        for (int j = 0; j < 1000; j++) {
+            Popul[i].Permutation(rnd);
+            Popul[i].Check();
+        }
+    }
+}
+
+
+void Population :: ChromoShow(){                            //show all genes of all chromos
+    for(int i=0;i<n_chromo;i++){
+        if(i<9) cout<<"Chromo "<<i+1<<":    ";
+        else if(i<99 & i>=9) cout<<"Chromo "<<i+1<<":   ";
+        else cout<<"Chromo "<<i+1<<":  ";
+        for(int j=0;j<n_genes;j++){
+            cout << setw(3) << Popul[i].GetGene(j);
+        }
+        cout<<endl;
+    }
+}
+
+void Population :: CitiesConfig(unsigned int k,int rank){ 
+    double theta;
+    vector<double>xcity(n_genes);
+    vector<double>ycity(n_genes);
+    
+    
+    
+    if(k==0){    
+        if(rank==0){
+        ofstream out;
+        out.open("../OUTPUT/Circle.dat");
+        for(int i=0;i<n_genes;i++){                         // cities around a circle r=1
+                
+                theta=2*M_PI*rnd.Rannyu();
+                xcity[i]=cos(theta);
+                ycity[i]=sin(theta);
+            
+                out<<xcity[i]<<","<<ycity[i]<<endl;
+            }
+            out<<xcity[0]<<","<<ycity[0]<<endl;
+            out.close();
+        }
+        MPI_Barrier(MPI_COMM_WORLD); // sincronizza tutti i processi qui  
+        MPI_Bcast(xcity.data(), n_genes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(ycity.data(), n_genes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        for(int i=0;i<n_genes;i++){ 
+            for(int j=0;j<n_genes;j++){
+                M(i,j)=Dist(xcity[i],ycity[i],xcity[j],ycity[j]);
+            }
+        }
+    }
+    else if(k==1){
+        if(rank==0){
+            ofstream out2;
+            out2.open("../OUTPUT/Square.dat");
+            for(int i=0;i<n_genes;i++){                         // random cities in a l=1 square
+                xcity[i]=rnd.Rannyu();
+                ycity[i]=rnd.Rannyu();
+                
+                out2<<xcity[i]<<","<<ycity[i]<<endl;
+            }
+            out2<<xcity[0]<<","<<ycity[0]<<endl;
+            out2.close();
+        } 
+        MPI_Barrier(MPI_COMM_WORLD); // sincronizza tutti i processi qui  
+        MPI_Bcast(xcity.data(), n_genes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(ycity.data(), n_genes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        for(int i=0;i<n_genes;i++){
+            for(int j=0;j<n_genes;j++){
+                M(i,j)=Dist(xcity[i],ycity[i],xcity[j],ycity[j]);
+            }
+        }
+            
+        
+    }
+
+    else if(k==2){
+        ifstream in;
+        in.open("../INPUT/europa.dat");
+        for(int i=0;i<n_genes;i++){
+            in>>xcity[i]>>ycity[i];
+        }
+        for(int i=0;i<n_genes;i++){
+            for(int j=0;j<n_genes;j++){
+                M(i,j)=Dist(xcity[i],ycity[i],xcity[j],ycity[j]);
+            }
+        }
+        in.close();
+    }
+
+    else if(k==3){
+        ifstream in;
+        in.open("../INPUT/cap_prov_ita.dat");
+        for(int i=0;i<n_genes;i++){
+            in>>xcity[i]>>ycity[i];
+        }
+        for(int i=0;i<n_genes;i++){
+            for(int j=0;j<n_genes;j++){
+                M(i,j)=Dist(xcity[i],ycity[i],xcity[j],ycity[j]);
+            }
+        }
+        in.close();
+    }
+    else{ 
+        cout<<"Error Simulation"<<endl;
+        exit(1);
+    }
+}
+
+void Population :: LossShow(){
+    for(int i=0;i<n_genes;i++){
+        for(int j=0;j<n_genes;j++){
+            cout<<fixed<<setprecision(1)<<M(i,j)<<setw(4);
+        }
+        cout<<endl;
+    }
+}
+
+void Population :: SetChromo(Chromosome c,unsigned int i){
+    for (int j=0;j<n_genes;j++){
+        Popul[i].SetGene(c.GetGene(j),j);
+    }
+}
+
+void Population :: LossChromoShow(){
+    for(int i=0;i<n_chromo;i++){
+        if(i<9) cout<<"Chromo "<<i+1<<":    ";
+        else if(i<99 & i>=9) cout<<"Chromo "<<i+1<<":   ";
+        else cout<<"Chromo "<<i+1<<":  ";
+        cout<<L(i)<<endl;
+    }
+}
+
+double Population :: L(unsigned int i){
+    double L=0;
+    unsigned int k,m;
+    for(int j=0;j<n_genes;j++){
+        k=Popul[i].GetGene(j);
+        m=Popul[i].GetGene(j+1);
+        L+=M(k-1,m-1);   //-1 dato che i geni vanno da 1 a 34 e gli indici di mat da 0 a 33
+    }
+    return L;
+}
+
+vector<Chromosome> Population :: Crossover(Chromosome c1, Chromosome c2) {
+    vector<Chromosome>C(2);
+    int r=rnd.RandInt(1,n_genes-1);
+    Chromosome s1(n_genes);
+    Chromosome s2(n_genes);
+    vector<bool>index1(n_genes,false);
+    vector<bool>index2(n_genes,false);
+    unsigned int g1;
+    unsigned int g2;
+
+    for(int i=0;i<r;i++) {
+        g1=c1.GetGene(i);
+        g2=c2.GetGene(i);
+
+        s1.SetGene(g1, i);
+        s2.SetGene(g2, i);
+
+        index1[g1]=true;
+        index2[g2]=true;
+    }
+
+    int idx1 = r, idx2 = r;
+    for(int i=0;i<n_genes&&(idx1<n_genes||idx2<n_genes);i++) {
+        unsigned int g_from_c2 = c2.GetGene(i);
+        unsigned int g_from_c1 = c1.GetGene(i);
+
+        if (idx1 < n_genes && !index1[g_from_c2]) {
+            s1.SetGene(g_from_c2, idx1++);
+            index1[g_from_c2] = true;
+        }
+
+        if (idx2 < n_genes && !index2[g_from_c1]) {
+            s2.SetGene(g_from_c1, idx2++);
+            index2[g_from_c1] = true;
+        }
+    }
+    s1.Check();
+    s2.Check();
+    C[0]=s1;
+    C[1]=s2;
+    return C;
+}
+
+
+void Population :: OrderChromo(){
+    vec Loss(n_chromo);
+    uvec Index(n_chromo);
+    vector<Chromosome>v(n_chromo);
+    for(int i=0;i<n_chromo;i++){
+        Loss[i]=L(i);
+    }
+    Index=sort_index(Loss);  //riordina in base alle loss salvando solo l' indice
+    for(int i=0;i<n_chromo;i++){
+        v[i]=Popul[Index[i]];
+        v[i].Check();
+    } 
+    Popul=v;
+}
+
+unsigned int Population :: Selection(){
+    double r=rnd.Rannyu();
+    double p=4;
+    unsigned int j=floor(n_genes*pow(r,p))+1;
+    return j;
+}
+
+void Population :: NewGeneration(double pc,double pm1,double pm2,double pm3,double pm4){
+    unsigned int m,n;
+    double sum=0;
+    double r;
+    int f;
+    vector<Chromosome>C(n_chromo);
+    vector<Chromosome>Help(2);
+    for(int i=0;i<n_chromo;i+=2){
+        m=Selection();
+        n=Selection();
+        r=rnd.Rannyu();
+        if(r<pc){
+            Help=Crossover(Popul[m],Popul[n]);
+            C[i]=Help[0];
+            C[i+1]=Help[1];
+        }
+        else{
+            C[i]=Popul[m];
+            C[i+1]=Popul[n];   
+        }
+        r=rnd.Rannyu();
+        
+        if(r<pm1){
+            C[i].Permutation(rnd);
+            C[i+1].Permutation(rnd);      
+        }
+        r=rnd.Rannyu();
+        if(r<pm2){
+            C[i].Shift(rnd);
+            C[i+1].Shift(rnd);      
+        }
+        r=rnd.Rannyu();
+        if(r<pm3){
+            C[i].Contamination(6,rnd);
+        }
+        r=rnd.Rannyu();
+        if(r<pm4){
+            C[i].Inversion(rnd);
+        }
+        
+    }
+    for(int i=0;i<n_chromo;i++){
+        SetChromo(C[i],i);
+    }
+}
+
+void Population::GetBestPath(unsigned int ex, int rank) {
+    string filename;
+    if (ex == 0) filename = "../OUTPUT/CirclePath" + to_string(rank) + ".dat";
+    else if (ex == 1) filename = "../OUTPUT/SquarePath" + to_string(rank) + ".dat";
+    else if (ex == 2) filename = "../OUTPUT/EuropePath" + to_string(rank) + ".dat";
+    else if (ex == 3) filename = "../OUTPUT/ItalyPath" + to_string(rank) + ".dat";
+    else {
+        cerr << "Invalid ex parameter\n";
+        exit(1);
+    }
+
+    ofstream out(filename);
+    if (!out.is_open()) {
+        cerr << "Error opening file " << filename << endl;
+        return;
+    }
+    for (int i = 0; i < n_genes; i++) {
+        out << Popul[0].GetGene(i) << endl;
+    }
+    out.close();
+}
+
+
+
+void ScambioBestChromosome(Population& Popul, int rank, int size, std::vector<int>& pairing, int M) {
+    int partner = pairing[rank];
+
+    std::vector<unsigned int> my_vec = Popul.GetChromo(0).ToVector();
+    std::vector<unsigned int> recv_vec(my_vec.size());
+
+    if (rank < partner) {
+        MPI_Send(my_vec.data(), my_vec.size(), MPI_UNSIGNED, partner, 0, MPI_COMM_WORLD);
+        MPI_Recv(recv_vec.data(), recv_vec.size(), MPI_UNSIGNED, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    } else {
+        MPI_Recv(recv_vec.data(), recv_vec.size(), MPI_UNSIGNED, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(my_vec.data(), my_vec.size(), MPI_UNSIGNED, partner, 0, MPI_COMM_WORLD);
+    }
+
+    Chromosome received;
+    received.FromVector(recv_vec);
+    Popul.SetChromo(received, M - 1);
+}
+
+
+
+
+
+
